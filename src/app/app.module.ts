066eb0cad80ee6f816d2163b04cdb0e9420fa951
Bairon Imbacuan <baiyeferson34@gmail.com>
2024-03-18 12:21:56 -0500

    add form login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
-import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 import { HeroPageComponent } from './auth/pages/hero-page/hero-page.component';
 import { ListPageComponent } from './auth/pages/list-page/list-page.component';
 import { NewPageComponent } from './auth/pages/new-page/new-page.component';
 import { SearchsPageComponent } from './auth/pages/searchs-page/searchs-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutPageComponent,
-    LoginPageComponent,
     HeroPageComponent,
     ListPageComponent,
     NewPageComponent,
@@ -25,9 +23,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
-    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
   declarations: [
-    RegisterPageComponent
+    RegisterPageComponent,
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
